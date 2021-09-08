0fe7c6b88a128152acf0098cef8aece879217a50
ANASS BOUCHFAR <anass.bouchfar22@gmail.com>
2021-09-08 14:08:26 +0100

    add infos

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">ANASS BOUCHFAR </span>
+            from <span className="purple"> Morocco.</span>
             <br />I am a junior pursuing IMSC in Maths and Computing in BIT
             Mesra.
             <br />
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,9 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
+          "Laravel & VueJs Developer",
+          "Data Engineer",
           "Open Source Contributor",
         ],
         autoStart: true,
