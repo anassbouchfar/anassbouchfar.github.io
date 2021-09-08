24d0b7112ab68d86dceded44a41814bad2157c10
ANASS BOUCHFAR <anass.bouchfar22@gmail.com>
2021-09-08 14:13:48 +0100

    infos

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -18,7 +18,7 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing GameS
             </li>
             <li className="about-activity">
               <ImPointRight /> Writting Tech Blogs
